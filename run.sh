3e727f74e953f92fc73399b52a0f0f23079504f1
Marcus Russi <marcusrussi@gmail.com>
2020-08-07 01:07:07 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#SBATCH --time=480
+#SBATCH --mail-user=marcus.russi@yale.edu
+#SBATCH --mail-type=ALL
+
+# Targets YCRC/Grace
+nextflow run covidestim/dailyFlow \
+  -profile ycrc \
+  -with-report report.html \
+  -with-timeline timeline.html \
+  -N "marcus.russi@yale.edu"
