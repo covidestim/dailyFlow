63a83d03c2dd75a87604ac2dd9374106458b584d
Marcus Russi <marcusrussi@gmail.com>
2020-09-15 23:23:19 -0400

    branch=SplinesRt for AWS runs

--- runAWS.sh
+++ runAWS.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
+JOBNAME="$(date '+%Y-%m-%d')"
+JOBQUEUE="highpriority-335bde00-db1b-11ea-9c54-02848c93abf4"
+
 # Targets AWS Batch
 aws batch submit-job \
-  --job-name "$(date '+%Y-%m-%d')" \
-  --job-queue "highpriority-335bde00-db1b-11ea-9c54-02848c93abf4" \
+  --job-name $JOBNAME \
+  --job-queue $JOBQUEUE \
   --job-definition nextflow \
   --container-overrides command=covidestim/dailyFlow,\
-"-profile","amazon","--branch","countymodel","--key","fips",\
-"--outdir","s3://nf-test-results/$(date '+%Y-%m-%d')"
+"-profile","amazon","--branch","SplinesRt","--key","fips",\
+"--outdir","s3://nf-test-results/$JOBNAME"
