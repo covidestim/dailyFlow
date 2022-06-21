4846169041176866166fe2375c18fd32ac1be3aa
Marcus Russi <marcusrussi@gmail.com>
2022-06-21 12:34:39 -0400

    Config changes for secrets, API changes
    - `NXF_ENABLE_SECRETS=true` for .sh scripts
    - Bumped required Nextflow version to min. required to use the new
      Secrets feature.
    - Got rid of some extraneous profiles
    - Added `db_local`, `db_prod` profiles, which pass the appropriate
      endpoints to the `outputs.nf` processes via environment variables

--- runLocal.sh
+++ runLocal.sh
@@ -3,6 +3,8 @@
 #SBATCH --mail-user=marcus.russi@yale.edu
 #SBATCH --mail-type=ALL
 
+export NXF_ENABLE_SECRETS=true
+
 # This script is for TESTING ONLY. In particular, the following changes should
 # be made when transitioning from testing to production:
 #
